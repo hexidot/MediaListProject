849a0085f7d55b23f22623aaf06d28845c69bd5f
hexidot <87951345+hexidot@users.noreply.github.com>
2023-12-19 19:28:34 -0600

    Initial commit

---
+++ src/main/java/com/vcolunga/SoloProjectProjectsAlgos/SoloProjectProjectsAlgosApplication.java
@@ -0,0 +1,13 @@
+package com.vcolunga.SoloProjectProjectsAlgos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloProjectProjectsAlgosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloProjectProjectsAlgosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcolunga/SoloProjectProjectsAlgos/SoloProjectProjectsAlgosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcolunga.SoloProjectProjectsAlgos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloProjectProjectsAlgosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
